c7dbf510417fbf1d5962372f50f2bb3c0e1d6d8f
Jonathan Spengeman <Jonathan Spengeman>
2014-09-18 15:06:41 -0700

    Custom user auth

---
+++ user_auth/migrations/0002_auto_20140917_0305.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('user_auth', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='myuser',
+            name='is_client',
+            field=models.BooleanField(default=False),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='myuser',
+            name='is_developer',
+            field=models.BooleanField(default=False),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='myuser',
+            name='is_project_manager',
+            field=models.BooleanField(default=False),
+            preserve_default=True,
+        ),
+    ]
---
+++ user_auth/models.py
@@ -0,0 +1,74 @@
+from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser
+)
+
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, password):
+        if not email:
+            raise ValueError('User must have an email')
+
+        if not password:
+            raise ValueError('User must have an password')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            password=password,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        if not email:
+            raise ValueError('User must have an email')
+
+        if not password:
+            raise ValueError('User must have an password')
+
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+        user.set_password(password)
+        user.is_admin = True
+        user.is_staff = True
+        user.is_active = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class MyUser(AbstractBaseUser):
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+    email = models.EmailField(max_length=256, unique=True)
+    date_joined = models.DateField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    #data memembers for permissions within the whole project
+    is_client = models.BooleanField(default=False)
+    is_project_manager = models.BooleanField(default=False)
+    is_developer = models.BooleanField(default=False)
+
+    objects = MyUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def get_full_name(self):
+        return self.email
+
+    def get_short_name(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    def full_name(self):
+        return self.first_name + " " + self.last_name
