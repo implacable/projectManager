d27212039df4c8c6d40a08de6ba6faf4fb4c8a6d
Justin Watt <justinwatt@gmail.com>
2014-09-19 15:09:59 -0700

    Able to create users. Passwords stored improperly.

--- user_auth/admin.py
+++ user_auth/admin.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from django import forms
 from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.contrib.auth.forms import ReadOnlyPasswordHashField, UserChangeForm
 from user_auth.models import MyUser
 
 
 class UserCreationForm(forms.ModelForm):
+
     password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
     password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
 
@@ -14,47 +15,33 @@ class UserCreationForm(forms.ModelForm):
         model = MyUser
         fields = ('email',)
 
-        
-    def clean(self):
-        cleaned_passwords = super(UserCreationForm, self).clean()
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and (password1 == password2):
-            return password2
-        else:
-            raise forms.ValidationError("Passwords don't match")
-
-    '''def clean_password2(self):
+    def clean_password2(self):
         # Check that the two password entries match
         password1 = self.cleaned_data.get("password1")
         password2 = self.cleaned_data.get("password2")
         if password1 and password2 and password1 != password2:
             raise forms.ValidationError("Passwords don't match")
-        return password2'''
+        else:
+            return password2
 
     def save(self, commit=True):
         # Save the provided password in hashed format
         user = super(UserCreationForm, self).save(commit=False)
-        user.set_password(self.cleaned_data["password1"])
+        user.set_password(self.cleaned_data["password2"])
         if commit:
             user.save()
         return user
 
 
 class UserChangeForm(forms.ModelForm):
-    password = ReadOnlyPasswordHashField(label=("Password")),
-
-    class Meta:
+    class Meta(UserChangeForm.Meta):
         model = MyUser
         fields = ('email', 'password', 'is_active', 'is_admin')
-
-    def clean_password(self):
-        return self.initial["password"]
 
 
 class MyUserAdmin(UserAdmin):
     form     = UserChangeForm
-    add_form = UserChangeForm
+    add_form = UserCreationForm
 
     list_display = ('email', 'is_admin')
     list_filter  = ('is_admin', )
@@ -66,13 +53,14 @@ class MyUserAdmin(UserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password',),}
+            'fields': ('email', 'password1','password2',),}
         ),
     )
 
     search_fields     = ('email',)
     ordering          = ('email',)
     filter_horizontal = ()
+    
 
 
 admin.site.register(MyUser, MyUserAdmin)
