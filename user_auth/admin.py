6914bde984739df758e72fdf6d69c8a0210f840d
Justin Watt <justinwatt@gmail.com>
2014-09-18 23:34:41 -0700

    Rewrote clean() to try to validate fields simulatenously. Possibly unneccesary.

--- user_auth/admin.py
+++ user_auth/admin.py
@@ -15,14 +15,22 @@ class UserCreationForm(forms.ModelForm):
         fields = ('email',)
 
         
+    def clean(self):
+        cleaned_passwords = super(UserCreationForm, self).clean()
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and (password1 == password2):
+            return password2
+        else:
+            raise forms.ValidationError("Passwords don't match")
 
-    def clean_password2(self):
+    '''def clean_password2(self):
         # Check that the two password entries match
         password1 = self.cleaned_data.get("password1")
         password2 = self.cleaned_data.get("password2")
         if password1 and password2 and password1 != password2:
             raise forms.ValidationError("Passwords don't match")
-        return password2
+        return password2'''
 
     def save(self, commit=True):
         # Save the provided password in hashed format
--- user_auth/models.py
+++ user_auth/models.py
@@ -14,6 +14,7 @@ class MyUserManager(BaseUserManager):
         user = self.model(
             email=self.normalize_email(email),
             password=password,
+            
         )
 
         user.set_password(password)
