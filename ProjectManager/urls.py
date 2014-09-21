2ccb424d003cc201ad219b5bbe8d2dd8af13eb3e
Jonathan Spengeman <Jonathan Spengeman>
2014-09-21 14:44:01 -0700

    Added a bunch of front end and a broken log in system

--- ProjectManager/urls.py
+++ ProjectManager/urls.py
@@ -2,9 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'ProjectManager.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+	url(r'^$', 'user_auth.views.home', name="home"),
+    url(r'^user/', include('user_auth.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
--- user_auth/views.py
+++ user_auth/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from user_auth.forms import LogIn
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def login(request):
+
+    if request.method == 'GET':
+        form = LogIn()
+    else:
+        form = LogIn(request.POST)
+        if form.is_valid():
+            user = authenticate(username=request.POST['email'],
+                                password=request.POST['password'])
+            login(request, user)
+
+            return HttpResponseRedirect(reverse('home'))
+
+    return render(request, 'user_auth/login.html', {'form': form})
+
+@login_required(login_url='login')
+def home(request):
+	return render(request, 'user_auth/index.html')
