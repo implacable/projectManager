379b4b111d32769df5e5d8c6cda26c91988235b7
jgtaruc <jgtaruc@gmail.com>
2014-09-30 15:01:37 -0700

    Fixed project and ticket class and register it to admin

--- project_ticket/admin.py
+++ project_ticket/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from project_ticket.models import Project, Ticket
+
 
 # Register your models here.
+class TicketInline(admin.TabularInline):
+	model = Ticket
+	extra = 0
+
+
+class ProjectAdmin(admin.ModelAdmin):
+	fieldsets = [
+	         ('None',                {'fields': ['project_name']}),
+	         ('Project Description', {'fields': ['description_of_project'], 'classes': ['collapse']}),
+	         ('Client',              {'fields': ['client']}),
+	         ('Project Manager',     {'fields': ['project_manager']}),
+	         ('Project Due',         {'fields': ['project_due']}),
+	     ]
+	inlines = [TicketInline]
+	
+
+admin.site.register(Project,ProjectAdmin)
--- project_ticket/models.py
+++ project_ticket/models.py
@@ -3,17 +3,16 @@ from user_auth.models import MyUser
 
 
 class Project(models.Model):
-    client = models.ManyToManyField(MyUser)
-    #project_manager = models.ForeignKey(MyUser)
-    name_project = models.CharField(max_length=32)
+    client = models.OneToOneField(MyUser, related_name = 'user_client')
+    project_manager = models.ManyToManyField(MyUser, related_name = 'user_project_manager')
+    project_name = models.CharField(max_length=32)
     project_due = models.DateField(blank=True, null=True)
-    project_description = models.CharField(max_length=256)
+    description_of_project = models.TextField(max_length=256)
 
 
 class Ticket(models.Model):
     developer = models.ManyToManyField(MyUser)
     project = models.ForeignKey(Project)
-    date_created = models.DateTimeField('Date Added')
-    comment_ticket = models.CharField(max_length=256)
+    date_created = models.DateField('Date Added')
     name_ticket = models.CharField(max_length=32)
 
