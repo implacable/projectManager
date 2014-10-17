3aaf19bbd52c71244b75aa925ece0dc9390ce244
Jonathan Spengeman <Jonathan Spengeman>
2014-10-16 20:08:28 -0700

    The URLS are now in /project_ticket instead of /user_auth

--- ProjectManager/urls.py
+++ ProjectManager/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-	url(r'^$', 'user_auth.views.home', name="home"),
-    url(r'^user/', include('user_auth.urls')),
+	url(r'^$', 'project_ticket.views.home', name="home"),
+    url(r'^user/', include('project_ticket.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
