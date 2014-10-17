e2ab65560e9933cfaffa62991112ebc05a79e979
Jonathan Spengeman <Jonathan Spengeman>
2014-10-16 20:47:26 -0700

    Adding the correct files

---
+++ project_ticket/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, include, url
+from project_ticket import views
+
+urlpatterns = patterns('',
+	url(r'^home$', views.home, name='home'),
+	url(r'^logout$', views.logout_view, name='logout'),
+	url(r'profile$', views.profile, name='profile'),
+    url(r'^$', views.login_view, name='login'),
+)
