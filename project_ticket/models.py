2110955fcd5560ebf39b3b8363a423d0ff609016
jgtaruc <jgtaruc@gmail.com>
2014-11-20 18:56:23 -0800

    working actionreport for Project,Ticket and Comment

--- project_ticket/models.py
+++ project_ticket/models.py
@@ -16,15 +16,16 @@ from datetime import datetime
 #					This new implementation might be silly or hacky?
 
 #			 To do:	
-#					1. Implement save override in Comments
+#					1. Implement save override in Comments/
 #					2. Changed variable names to something more descriptive
 #						i.e. old,verb,new
-#					3. Check the logic of save() in Project
+#					3. Check the logic of save() in Project/
 #					4. Improve the verb(s) values(message)
-#				*	5. Error with line 41 when creating a new project
+#				*	5. Error with line 41 when creating a new project/
 #					6. Refactor
 #					7. Delete this comment after ActionReport is fully functional on
 #						all models
+#					8. change default value of ticket to Queud
 class Project(models.Model):
 	client = models.OneToOneField(MyUser, related_name = 'client')
 	project_manager = models.ManyToManyField(MyUser, related_name = 'project_manager')
@@ -38,23 +39,14 @@ class Project(models.Model):
 
 	def save(self):
 		action = ActionReport()
+		action.project = self
 		if self.pk is not None:
-			action.project = Project.objects.get(pk=self.pk)
 			old = Project.objects.get(pk=self.pk)
-			if self.name != old.name or self.description != old.description:
-				verb =" modified %s " % (old.name)
+			if self.name != old.name or self.description != old.description or self.project_due != old.project_due:
+				verb =" edited %s " % (old.name)
 				action.message = verb + "%s" % (datetime.now()).strftime(" %B %d, %Y at %l:%M.")
 				action.save()
-				super(Project, self).save()
-		elif not self.pk:
-			verb = " created %s" % (self.name)
-			action.message = verb + "%s" % (datetime.now()).strftime(" %B %d, %Y at %l:%M.")
-			super(Project, self).save()
-#	action.message =... and action.save() and super(Project...) lines were moved and duplicated for the if/else statement to 
-#	cover the instance when the user click save even if the user did not change any information
-#	Otherwise: There will be a variable error "verb was referenced before assignment"
-
-#	To do: When creating a new project there is an error with action.sav
+		super(Project, self).save()
 
 
 class Ticket(models.Model):
@@ -84,13 +76,19 @@ class Ticket(models.Model):
 			old = Ticket.objects.get(pk=self.pk)
 			if old.status != self.status:
 				verb = " changed %s from %s to %s on " % (self.name, old.status, self.status)
-			if old.name != self.name:
-				verb = " change %s name to %s on " % (old.name,self.name)
+				action.message = verb + "%s" % (datetime.now()).strftime(" %B %d, %Y at %l:%M.")
+				action.save()
+			if old.name != self.name or old.description_ticket != self.description_ticket:
+				verb = " edited %s" % (self.name)
+				action.message = verb + "%s" % (datetime.now()).strftime(" %B %d, %Y at %l:%M.")
+				action.save()
+			
 		else:
 			verb =  " added %s to %s in %s on " % (self.name,self.status,self.project.name)    	
-		action.message = verb + "%s" % (datetime.now()).strftime(" %B %d, %Y at %l:%M.")
-		action.save()
+			action.message = verb + "%s" % (datetime.now()).strftime(" %B %d, %Y at %l:%M.")
+			action.save()
 		super(Ticket, self).save()
+
 
 # Insert Comment Class here!
 class Comment(models.Model):
@@ -104,13 +102,15 @@ class Comment(models.Model):
     	action.project = self.ticket.project
     	if self.pk is not None:
     		old = Comment.objects.get(pk=self.pk)
-    		if old.text != self.text:
-    			verb = " edited comment on %s on " % (self.ticket.name)
+    		if (old.text != self.text or old.date_submitted != self.date_submitted 
+    			or self.user != old.user or old.ticket != self.ticket):
+    			verb = "%s edited comment on %s on " % (self.user, self.ticket.name)
     			action.message = verb + "%s" % (datetime.now()).strftime(" %B %d, %Y at %l:%M")
-	    		action.save()
-    			super(Comment, self).save()
+    			action.save()
+    			super(Comment,self).save()
+
     	else:
-    		verb = " commented on %s on " % (self.ticket.name)
+    		verb = "%s commented on %s on " % (self.user, self.ticket.name)
     		action.message = verb + "%s" % (datetime.now()).strftime(" %B %d, %Y at %l:%M")
     		action.save()
     		super(Comment,self).save()
@@ -127,28 +127,6 @@ class ActionReport(models.Model):
     project = models.ForeignKey(Project, related_name="projects")
     message = models.CharField(max_length= 32, default=" ")
 
-#################	This is the old implementation fo create_msg()	#################
-#    def create_msg(self,verb):
-#    	if md == "ticket":
-#    		if new.pk is not None:
-#				old = Ticket.objects.get(pk=new.pk)
-#				if old.status != new.status:
-#					verb = " changed %s from %s to %s on " % (new.name, old.status, new.status)
-#				if old.name != new.name:
-#					verb = " change %s name to %s on " % (old.name,new.name)
-#				if old.project.name != new.project.name:
-#					verb = "111"	
-#    		else:
-#	    		verb =  " added %s to %s in %s on " % (new.name,new.status,new.project.name)    	
-#			if new.pk is not None:
-#				old = Project.objects.get(pk=new.pk)
-#				if old.name != new.name:
-#					verb = " modified %s" % (old.name)
-#			else:
-#				verb = " created a project"		
-#		self.message = verb + "%s" % (datetime.now()).strftime(" %B %d, %Y at %l:%M.")
-#######################################################################################
     def save(self):
-    	#self.create_msg(verb)
     	super(ActionReport, self).save()
 
